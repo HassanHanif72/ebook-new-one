bfc065ca6de97450cb8de35acc0798b1a2a364c0
hassanhanif72 <hassanhanif72@gmail.com>
2025-11-13 04:14:04 +0500

    Upload

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,5 +1,21 @@
 export default function Home() {
   return (
-    <h1>sfd</h1>
+    <>
+      {/* top banner */}
+      <section className="top-banner">
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-xl-6 col-lg-6 col-md-12">
+              <div className="center-col">
+                <h1>Let Us Help You Write Best Selling Business Books</h1>
+                <p className="mb-4">Avail the best ghost-writing services from our affordable self-publishing ghostwriting agency. We are experts in creating fiction, non-fiction, mysteries.</p>
+                <button className="btn btn-theme">Get Started </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* end */}
+    </>
   );
 }
---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,139 @@
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react'
+
+
+const Header = () => {
+
+    // sticky / hover state
+    const [isSticky, setIsSticky] = useState(false);
+    const [isHover, setIsHover] = useState({
+        services: false,
+        genre: false
+    });
+
+    // hover dropdown
+    const handleMouseEnter = (dropdownName) => {
+        setIsHover(prevState => ({
+            ...prevState,
+            [dropdownName]: true
+        }))
+    }
+    const handleMouseLeave = (dropdownName) => {
+        setIsHover(prevState => ({
+            ...prevState,
+            [dropdownName]: false
+        }));
+    }
+    // end
+
+    // add sticky class
+    useEffect(() => {
+        const handleScrool = () => {
+            if (window.scrollY > 0) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false)
+            }
+        }
+        window.addEventListener('scroll', handleScrool)
+        handleScrool();
+        return () => {
+            window.removeEventListener('scroll', handleScrool)
+        }
+    }, []);
+    // end
+
+    // Off canvas
+    const [offcanvasShow, setOffcanvasShow] = useState(false);
+    const handleClose = () => setOffcanvasShow(false);
+    const handleShow = () => setOffcanvasShow(true);
+    // end
+
+    // Modal context
+    // const { modalShow } = useModal();
+    // end
+
+    // active link logic
+    const pathname = usePathname();
+    // end
+
+    // hide offcanvas when click
+    useEffect(() => {
+        setOffcanvasShow(false);
+    }, [pathname])
+    // end
+
+    return (
+        <>
+            <header className={`header ${isSticky ? 'sticky' : ''}`}>
+                <nav className="navbar navbar-expand-xl navbar-dark stroke bg-transparent">
+                    <div className="container">
+                        <Link className="navbar-brand" href="/">
+                            <Image src="/assets/images/logo.png" alt="logo" width={112} height={48} />
+                        </Link>
+                        <button className="navbar-toggler" type="button" onClick={handleShow} aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${pathname === '/' ? 'active' : ''}`} href="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${pathname === '/about-us' ? 'active' : ''}`} href="/about-us">About Us</Link>
+                                </li>
+                                <li className="nav-item dropdown navdesktop">
+                                    <Link className={`nav-link ${pathname === '#' ? 'active' : ''}`} href="#" onMouseEnter={() => handleMouseEnter('services')} onMouseLeave={() => handleMouseLeave('services')}>
+                                        Services <i className="ri-arrow-down-s-line"></i>
+                                    </Link>
+                                    <ul className={`dropdown-menu ${isHover.services ? 'show' : ''}`}
+                                        onMouseEnter={() => handleMouseEnter('services')}
+                                        onMouseLeave={() => handleMouseLeave('services')} aria-labelledby="navbarDropdown">
+                                        <li><Link className="dropdown-item" href="/ebook-writing">eBook Writing</Link></li>
+                                        <li><Link className="dropdown-item" href="/ebook-editing">eBook Editing</Link></li>
+                                        <li><Link className="dropdown-item" href="/ebook-marketing">eBook Marketing</Link></li>
+                                        <li><Link className="dropdown-item" href="/ebook-publishing">eBook Publishing</Link></li>
+                                        <li><Link className="dropdown-item" href="/ebook-cover">eBook Cover</Link></li>
+                                        <li><Link className="dropdown-item" href="/children-book">Children Book</Link></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <Link className="nav-link" href="#" onMouseEnter={() => handleMouseEnter('genre')} onMouseLeave={() => handleMouseLeave('genre')}>
+                                        Genre <i className="ri-arrow-down-s-line"></i>
+                                    </Link>
+                                    <ul className={`dropdown-menu ${isHover.genre ? 'show' : ''}`}
+                                        onMouseEnter={() => handleMouseEnter('genre')}
+                                        onMouseLeave={() => handleMouseLeave('genre')}
+                                        aria-labelledby="navbarDropdown">
+                                        <li><Link className="dropdown-item" href="/genre/autobiography">Autobiography</Link> </li>
+                                        <li><Link className="dropdown-item" href="/genre/business">Business</Link></li>
+                                        <li><Link className="dropdown-item" href="/genre/fiction">Fiction</Link> </li>
+                                        <li><Link className="dropdown-item" href="/genre/mystery-book">Mystery Book</Link></li>
+                                        <li><Link className="dropdown-item" href="/genre/non-fiction">Non-Fiction</Link></li>
+                                        <li><Link className="dropdown-item" href="/genre/novel-writing">Novel Writing</Link> </li>
+                                        <li><Link className="dropdown-item" href="/genre/self-help">Self Help</Link></li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${pathname === '/contact-us' ? 'active' : ''}`} href="/contact-us">
+                                        Contact Us
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="d-xl-block d-none">
+                            <button className="btn btn-theme">Get Started </button>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            {/* {offcanvasShow && <OffCanvas show={offcanvasShow} handleClose={handleClose} pathname={pathname} />} */}
+        </>
+    )
+}
+
+export default Header
